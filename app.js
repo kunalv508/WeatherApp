51034ddedc919cd225c433471848108c0f72fc0b
kunalv508 <kunalv508@gmail.com>
2017-04-02 13:20:45 -0700

    Update app.js

--- app.js
+++ app.js
@@ -14,7 +14,16 @@ form.addEventListener("submit", function(){
 	
 	//console.log();
 	//return false;
+	function getDay(day) {
+	 let days = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THURSDAY", "FRIDAY", "SATURDAY"]
+	 return (days[day])
+	}
 
+	function getMonth(month) {
+	 let months = ["JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"]
+	 return (months[month])
+
+	}
 	
 	let citycode = '';
    	let countrycode = '';
@@ -31,56 +40,58 @@ form.addEventListener("submit", function(){
     	if(citycode !='' && countrycode !='') {
     		let myHeaders = new Headers();
 			let myInit = { method: 'GET',
-			               headers: myHeaders,
-			               cache: 'default',
-			               dataType : "jsonp",
-			           	};         	
+			              headers: myHeaders,
+			              cache: 'default',
+			              dataType : "jsonp",
+			          	};         	
 			if(document.getElementById("future").checked){   //to check if 5 days forcast is required or not        	
-				fetch('http://api.openweathermap.org/data/2.5/forecast?q='+citycode+','+countrycode+'&units='+units+'&appid=ae90958c318f907c4f35609efd85e9c3', myInit)
+				fetch('http://api.openweathermap.org/data/2.5/forecast/daily?q='+citycode+','+countrycode+'&units='+units+'&appid=ae90958c318f907c4f35609efd85e9c3', myInit)
 				.then(function(response) { return response.json(); })
-			    .then(function(weather) {
-			    	//console.log(weather);
-			    	let htmlData = '<ul>';
+			   .then(function(weather) {
+			   	//console.log(weather);
+			   	let htmlData = '';
 					for(let i = 0; i < weather.list.length; i++) {
-
-						htmlData +='<li><h3>Date : '+weather.list[i].dt_txt+'</h3></li>';
-						htmlData +='<li>Atmospheric pressure on the ground level : '+weather.list[i].main.grnd_level+'</li>';
-						htmlData +='<li>Atmospheric pressure on the sea level by default : '+weather.list[i].main.pressure+'</li>';
-						htmlData +='<li>Atmospheric pressure on the sea level : '+weather.list[i].main.sea_level+'</li>';
-						htmlData +='<li>Temperature '+units+': '+weather.list[i].main.temp+'</li>';
-						htmlData +='<li>Humidity %: '+weather.list[i].main.humidity+'</li>';
-						htmlData +='<li> Maximum temperature at the moment of calculation : '+weather.list[i].main.temp_max+'</li>';
-						htmlData +='<li>Minimum temperature at the moment of calculation : '+weather.list[i].main.temp_min+'</li>';
-						htmlData +='<li><img src="http://openweathermap.org/img/w/'+weather.list[i].weather['0'].icon+'.png"></li>';
-						htmlData +='<li>Weather : '+weather.list[i].weather['0'].description+'</li>';
-
+						htmlData +='<ul>';
+						let date = new Date((weather.list[i].dt) * 1000);
+						htmlData +='<li class="text-c"><h4>'+getDay(date.getDay())+'</h4></li>';
+						htmlData +='<li class="text-c"><h4>'+date.getDate() + " " + getMonth(date.getMonth())+'</h4></li>';
+						htmlData +='<li class="text-c wet-c">'+weather.list[i].weather['0'].description+'</li>';
+						htmlData +='<li class="text-c"><img src="http://openweathermap.org/img/w/'+weather.list[i].weather['0'].icon+'.png"></li>';
+						htmlData +='<li>Pressure : '+weather.list[i].pressure+'</li>';
+						htmlData +='<li>Wind '+weather.list[i].speed+'</li>';
+						htmlData +='<li>Humidity %: '+weather.list[i].humidity+'</li>';
+						htmlData +='<li>Max temp. : '+weather.list[i].temp.max+'</li>';
+						htmlData +='<li>Min temp. : '+weather.list[i].temp.min+'</li>';
+						htmlData +='</ul>';
 					}
-					htmlData +='</ul>';
+					
 					document.getElementById('display').innerHTML = htmlData;
 					//console.log(htmlData);
-			    })
+			   })
 				.catch(function(error) {  
 				console.log('Fetch Error :-S'+error);  
 				});
 			}else{
 				fetch('http://api.openweathermap.org/data/2.5/weather?q='+citycode+','+countrycode+'&units='+units+'&appid=ae90958c318f907c4f35609efd85e9c3', myInit)
 				.then(function(response) { return response.json(); })
-			    .then(function(weather) {
-			    	//console.log(weather);
-			    	let htmlData = '<ul>';
-			      	htmlData +='<li><h3>Date : Today</h3></li>';
-					htmlData +='<li>Atmospheric pressure on the sea level by default : '+weather.main.pressure+'</li>';
+			   .then(function(weather) {
+			   	//console.log(weather);
+			   	let htmlData = '<ul>';
+			     	htmlData +='<li class="text-c"><h4>Date : Today</h4></li>';
+			     	htmlData +='<li class="text-c wet-c">'+weather.weather['0'].description+'</li>';
+					htmlData +='<li class="text-c"><img src="http://openweathermap.org/img/w/'+weather.weather['0'].icon+'.png"></li>';
+					
 					htmlData +='<li>Temperature '+units+': '+weather.main.temp+'</li>';
 					htmlData +='<li>Humidity %: '+weather.main.humidity+'</li>';
-					htmlData +='<li> Maximum temperature at the moment of calculation : '+weather.main.temp_max+'</li>';
-					htmlData +='<li>Minimum temperature at the moment of calculation : '+weather.main.temp_min+'</li>';
-					htmlData +='<li><img src="http://openweathermap.org/img/w/'+weather.weather['0'].icon+'.png"></li>';
-					htmlData +='<li>Weather : '+weather.weather['0'].description+'</li>';
+					htmlData +='<li> Max temp. : '+weather.main.temp_max+'</li>';
+					htmlData +='<li>Min temp. : '+weather.main.temp_min+'</li>';
 
-			     
-			      	htmlData +='</ul>';
-			      	document.getElementById('display').innerHTML = htmlData;
-			    })
+					
+
+			    
+			     	htmlData +='</ul>';
+			     	document.getElementById('display').innerHTML = htmlData;
+			   })
 				.catch(function(error) {  
 				console.log('Fetch Error :-S'+error);  
 				});
